43dc249dad9546d12a7d77e47d23eedc7044493f
msho <mayo_s@hotmail.co.uk>
2023-12-03 13:06:00 +0000

    started frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accounts, setAccounts] = useState([]);
+  const [proxies, setProxies] = useState([]);
+  const [scrapeData, setScrapeData] = useState([]);
+
+  const [proxy, setProxy] = useState([]);
+
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+
+
+
+  const handleStartScrape = () => {}
+
+  const handleSubmitProxy = () => {}
+
+  const handleUploadAccount = () => {}
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={handleStartScrape}> start scrape</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <form onSubmit={handleSubmitProxy}>
+          <label htmlFor="proxy">Proxy:</label>
+          <input type="text" id="proxy" value={proxy} onChange={(e) => setProxy(e.target.value)}/>
+          <input type="submit">Upload Proxy</input>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <form onSubmit={handleUploadAccount}>
+          <label htmlFor="email">Upload Account:</label>
+          <input type="text" id="email" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+
+          <label htmlFor="password">Upload Account:</label>
+          <input type="text" id="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+          <input type="submit">Upload Account</input>
+        </form>
+      </div>
+
+      <div>
+        <div>
+          <div>Accounts</div>
+        </div>
+
+        <div>
+          <div>proxies</div>
+        </div>
+
+        <div>
+          <div>scraped data</div>
+        </div>
+      </div>
     </>
   )
 }
