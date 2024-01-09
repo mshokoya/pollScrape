970f0de4e838d8e46ee06e0aa312de2ed737151b
msho <mayo_s@hotmail.co.uk>
2024-01-09 12:57:52 +0000

    -

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,12 +21,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/addaccount', async (req, res) => {
-  console.log('addAccount')
+  console.log('addAccount');
   try {
     // await addAccountToDB(req.body.email, req.body.password)
-    res.json({ok: true, message: null, data: null})
+    res.json({ok: true, message: null, data: null});
   } catch (err) {
-    res.json({ok: false, message: 'failed to add user', data: err})
+    res.json({ok: false, message: 'failed to add user', data: err});
   }
 });
 
