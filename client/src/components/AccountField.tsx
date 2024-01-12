89e928b530e939133c366391699ad77af3fe4a01
msho <mayo_s@hotmail.co.uk>
2024-01-12 08:41:04 +0000

    overflow in flexbox working

--- client/src/components/AccountField.tsx
+++ client/src/components/AccountField.tsx
@@ -43,64 +43,66 @@ export const AccountField = () => {
   }
 
   return (
-    <div className="flex flex-col grow">
-      <div className='mb-10'>
-        <form onSubmit={handleSubmit}>
-          <div className='mb-3'>
-            <label className='mr-2 border-cyan-600 border-b-2' htmlFor="email">Email:</label>
-            <input required type="text" id="email" value={input.email} onChange={ e => {setInput(p => ({...p, email: e.target.value}))}}/>
-          </div>
+    <div className="flex relative grow">
+      <div className="flex flex-col grow absolute inset-x-0 inset-y-0">
+        <div className='mb-10'>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+              <label className='mr-2 border-cyan-600 border-b-2' htmlFor="email">Email:</label>
+              <input required type="text" id="email" value={input.email} onChange={ e => {setInput(p => ({...p, email: e.target.value}))}}/>
+            </div>
 
-          <div className='mb-3'>
-            <label className='mr-2 border-cyan-600 border-b-2' htmlFor="password">Password:</label>
-            <input required type="text" id="password" value={input.password} onChange={ e => {setInput(p => ({...p, password: e.target.value}))}}/>
-          </div>
+            <div className='mb-3'>
+              <label className='mr-2 border-cyan-600 border-b-2' htmlFor="password">Password:</label>
+              <input required type="text" id="password" value={input.password} onChange={ e => {setInput(p => ({...p, password: e.target.value}))}}/>
+            </div>
 
-          <input className='text-cyan-600 border-cyan-600 border rounded p-1 mt-3' type="submit" value="Add Account"/>
-        </form>
-      </div>
-      
-      <div className='border-cyan-600 border rounded grow'>
-        <table className="text-[0.7rem] m-auto w-full table-fixed">
-          <thead>
-            <tr>
-              <th>Trial</th>
-              <th>Email</th>
-              <th>Password</th>
-              <th className='w-[10%]'><IoOptionsOutline className='inline' /></th>
-            </tr>
-          </thead>
-          <tbody className="text-[0.5rem]" onClick={handleExtendRow}>
-            {
-              accounts.length && accounts.map( 
-                (a, idx) => ( 
-                  <>
-                    <tr className='text-center' key={idx}>
-                      <td className='overflow-scroll'>{a.trialTime.toDateString()}</td>
-                      <td className='overflow-scroll'>{a.email}</td>
-                      <td className='overflow-scroll'>{a.password}</td>
-                      <td className='overflow-scroll' ><SlOptionsVertical className='inline'/></td>
-                    </tr>
-                    <tr>
-                      <td colSpan={3} className="hidden">
-                        <div>_id: {a._id}</div>
-                        <div>domain: {a.domain}</div>
-                        <div>accountType: {a.accountType}</div>
-                        <div>trialTime: {a.trialTime.toDateString()}</div>
-                        <div>isSuspended: {a.isSuspended}</div>
-                        <div>email: {a.email}</div>
-                        <div>password: {a.password}</div>
-                        <div>cookies: {a.cookies}</div>
-                        <div>proxy: {a.proxy}</div>
-                        <div>lastUsed: {a.lastUsed.toDateString()}</div>
-                      </td>
-                    </tr>
-                  </>
+            <input className='text-cyan-600 border-cyan-600 border rounded p-1 mt-3' type="submit" value="Add Account"/>
+          </form>
+        </div>
+        
+        <div className='border-cyan-600 border rounded grow overflow-auto'>
+          <table className="text-[0.7rem] m-auto w-full table-fixed">
+            <thead>
+              <tr>
+                <th>Trial</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th className='w-[10%]'><IoOptionsOutline className='inline' /></th>
+              </tr>
+            </thead>
+            <tbody className="text-[0.5rem]" onClick={handleExtendRow}>
+              {
+                accounts.length && accounts.map( 
+                  (a, idx) => ( 
+                    <>
+                      <tr className='text-center' key={idx}>
+                        <td className='overflow-scroll'>{a.trialTime.toDateString()}</td>
+                        <td className='overflow-scroll'>{a.email}</td>
+                        <td className='overflow-scroll'>{a.password}</td>
+                        <td className='overflow-scroll' ><SlOptionsVertical className='inline'/></td>
+                      </tr>
+                      <tr>
+                        <td colSpan={3} className="hidden">
+                          <div>_id: {a._id}</div>
+                          <div>domain: {a.domain}</div>
+                          <div>accountType: {a.accountType}</div>
+                          <div>trialTime: {a.trialTime.toDateString()}</div>
+                          <div>isSuspended: {a.isSuspended}</div>
+                          <div>email: {a.email}</div>
+                          <div>password: {a.password}</div>
+                          <div>cookies: {a.cookies}</div>
+                          <div>proxy: {a.proxy}</div>
+                          <div>lastUsed: {a.lastUsed.toDateString()}</div>
+                        </td>
+                      </tr>
+                    </>
+                  )
                 )
-              )
-            }
-          </tbody>
-        </table>
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
