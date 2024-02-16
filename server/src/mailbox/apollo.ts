9b9c7a22182a635c8d16211dc2c29e425257358e
msho <mayo_s@hotmail.co.uk>
2024-02-16 17:52:36 +0000

    confirm apollo account WIP 2

---
+++ server/src/mailbox/apollo.ts
@@ -0,0 +1,15 @@
+import { FetchMessageObject } from "imapflow";
+import { simpleParser } from "mailparser";
+
+export const getApolloConfirmationLinksFromMail = async (mail: FetchMessageObject) => {
+  const arr = []
+  const parsedData = await simpleParser(mail.source);
+  
+  const link1 = parsedData.text?.match(/(?<=Activate Your Account \( )[\S|\n]+/g)
+  const link2 = parsedData.text?.match(/(?<=Or paste this link into your browser: )[\S|\n]+(?= \()/g)
+
+  if (link1 && link1.length) arr.push(link1[0].replace('\n', '').replace('\r', ''))
+  if (link2 && link2.length) arr.push(link2[0].replace('\n', '').replace('\r', ''))
+
+  return arr
+}
