801b9c27e74193a4996fce66c49ddf405c55f828
msho <mayo_s@hotmail.co.uk>
2024-03-25 22:03:53 +0000

    core working

--- ugly-scraper/src/main/index.ts
+++ ugly-scraper/src/main/index.ts
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import startBE from '../main/start'
+import startBE from './start.js'
 
 function createWindow(): void {
   // Create the browser window.
