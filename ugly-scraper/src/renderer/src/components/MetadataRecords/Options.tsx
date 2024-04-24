c89261114de05d048c5559f229b8c472ca43e8ec
msho <mayo_s@hotmail.co.uk>
2024-04-24 23:12:28 +0100

    -

---
+++ ugly-scraper/src/renderer/src/components/MetadataRecords/Options.tsx
@@ -0,0 +1,39 @@
+import { ObservableComputed } from '@legendapp/state'
+import { Button, DropdownMenu } from '@radix-ui/themes'
+import { downloadData } from '@renderer/core/util'
+import { IRecords } from '@shared/index'
+
+export const Options = ({
+  filteredRecords
+}: {
+  filteredRecords: ObservableComputed<IRecords[]>
+}) => {
+  return (
+    <div className="w-3 mb-1">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Button variant="soft" color="indigo">
+            Options
+            <DropdownMenu.TriggerIcon />
+          </Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Sub>
+            <DropdownMenu.SubTrigger>Download</DropdownMenu.SubTrigger>
+            <DropdownMenu.SubContent>
+              <DropdownMenu.Item onClick={() => downloadData(filteredRecords.get(), 'json')}>
+                JSON
+              </DropdownMenu.Item>
+              <DropdownMenu.Item onClick={() => downloadData(filteredRecords.get(), 'csv')}>
+                CSV
+              </DropdownMenu.Item>
+            </DropdownMenu.SubContent>
+          </DropdownMenu.Sub>
+          <DropdownMenu.Item>Select all</DropdownMenu.Item>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item color="red">Delete Selected</DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
+  )
+}
